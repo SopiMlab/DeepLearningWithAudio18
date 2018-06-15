f817789849e5811aa0a5f45ec7fbde358c55d289
Vapaakallio Lassi <lassi.vapaakallio@aalto.fi>
2018-06-15 17:10:39 +0300

    well, now there's numpy arrays

--- basic_classification.py
+++ basic_classification.py
@@ -1,10 +1,12 @@
 import os
+import numpy as np
 from pydub import AudioSegment
 
-sounds_dir = "input/categorized/car_horn" 
+sounds_dir = "input/categorized/car_horn/" 
 wav_fps = os.listdir(sounds_dir)
 
 for wav in wav_fps:
     sound = AudioSegment.from_wav(sounds_dir + wav)
     array = sound.get_array_of_samples()
-    print(array)
+    nparray = np.array(array)
+    print(nparray.shape)
