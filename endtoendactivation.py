bf9329e4735ba384b7323da8991554b25ba4b409
Lassi Vapaakallio <lassi.vapaakallio@aalto.fi>
2018-06-28 16:35:15 +0300

    I don't even know. Waveforms are tricky.

--- endtoendactivation.py
+++ endtoendactivation.py
@@ -5,35 +5,32 @@ from keras.layers.advanced_activations import LeakyReLU
 from keras.layers import Input, Dense, UpSampling1D, Conv1D, Activation, Reshape, BatchNormalization
 import numpy as np
 from playsound import play_sound, play_and_save_sound
+from audio_loader import load_all
 
 latent_dim = 100
-sound = AudioSegment.from_file('input/categorized/cat/1-34094-A-5.wav')
-#sound = AudioSegment.from_wav("input/speech_commands/bird/0a7c2a8d_nohash_1.wav")
-samples = int(sound.frame_count())
-sound = sound.set_channels(1) 
-soundarray = sound.get_array_of_samples()
-target = []
-target.append(np.array(soundarray))
-target = np.array(target)
-print(target.shape)
 
-#target = target.reshape(target.shape[0], target.shape[1], 1)
+#sound = AudioSegment.from_wav("input/speech_commands/bird/0a7c2a8d_nohash_1.wav")
+sound = load_all("categorized", "cat",forceLoad=True)
+sound = sound / 65536
+target = np.array(sound[0])
+print(target.shape)
 
 input_shape = (target.shape[1],1)
 
-play_sound(target, "original")
+play_and_save_sound(sound, "endtoend2", "original", upscale=True)
 
 model = Sequential()
 
-model.add(Activation("tanh", input_shape=input_shape))
+model.add(Activation("sigmoid", input_shape=input_shape))
 
 model.summary()
 
-noise = Input(shape=(samples,))
+noise = Input(shape=(len(sound[0]),))
 clip = model(noise)
 
 generator = Model(noise, clip)
 
+target = target.reshape(target.shape[1], target.shape[0])
 gen_clip = generator.predict(target, 1)
 
-play_sound(gen_clip, "generated")
+play_and_save_sound(gen_clip, "endtoend2", "sigmoidactivated", upscale=True)
--- playsound.py
+++ playsound.py
@@ -16,23 +16,27 @@ def play_sound(sample, label, upscale=False): # We don't know what the original 
     print("playing sound from category " + str(label))
     play(new_sound)
 
-def play_and_save_sound(samples, label, run_name="", epoch=0):
+def play_and_save_sound(samples, label, run_name="", epoch=0, upscale=True):
     check_sample(samples[0])
     #sound = AudioSegment.from_file('input/speech_commands/bed/1bb574f9_nohash_0.wav')
     sound = AudioSegment.from_file('input/categorized/cat/1-34094-A-5.wav')
     sound.set_channels(1)
     print(sound.array_type)
-    upscaled_array = array.array(sound.array_type, upscale_sample(samples[0]))
-    new_sound = sound._spawn(upscaled_array)
-    #print(upscaled_array)
-    plt.plot(upscaled_array)
+    playsound = samples[0]
+    if upscale:
+        playsound = upscale_sample(playsound)
+    sample_array = array.array(sound.array_type, playsound)
+    new_sound = sound._spawn(sample_array)
+    if not os.path.exists("output/" + label + "/"):
+        os.makedirs("output/" + label + "/")
+    #print(sample_array)
+    plt.figure(figsize=(30,10))
+    plt.plot(sample_array)
     plt.savefig("output/" + label + "/" + run_name + "#" + str(epoch))
     plt.clf()
     plt.cla()
     print("playing and saving sound from category " + str(label))
     play(new_sound)
-    if not os.path.exists("output/" + label + "/"):
-        os.makedirs("output/" + label + "/")
     new_sound.export("output/" + label + "/" + run_name + "#" + str(epoch) + ".wav", format="wav")
 
 def save_sound(sample, label):
@@ -64,14 +68,14 @@ def check_scale (sample):
             maxi = i
     return max(abs(maxi), abs(mini))
     
-def upscale_sample(sample):
+def upscale_sample(sample): 
     check_sample(sample)
     scale = check_scale(sample)
     print("scale {}".format(scale))
-    if(scale > 1):
-        new_sample = (sample /scale) * 32767
+    if(scale > 0.5): # this is a terrible approach that makes no sense. Also it shouldn't get over the limit anyway. But it does, so no wonder the GAN goes nuts.
+        new_sample = (sample /scale) * 32768
         print("went over 1, this should be impossible")
     else:
-        new_sample = sample * 32767
+        new_sample = sample * 65536
     check_sample(new_sample)
-    return new_sample
+    return new_sample.astype(int)
